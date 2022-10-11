64fb13414a6a5d43ce0ec843440d97af887fe592
Gabriel Hernandes <gabrielmh13@hotmail.com>
2022-10-11 11:54:10 -0300

    Versão final

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.main`
+    flex: 1;
+    
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 3.5rem;
+    }
+`
+
+const BaseCountdownButton = styled.button`
+    width: 100%;
+    border: none;
+    padding: 1rem;
+    border-radius: 8px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: ${props => props.theme['gray-100']};
+
+
+    gap: 8px;
+    font-weight: bold;
+
+    cursor: pointer;
+
+    
+
+    &:disabled {
+        opacity: 0.7;
+        cursor: not-allowed;
+    }
+
+    
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+    background: ${props => props.theme['green-500']};
+
+    &:not(:disabled)hover{
+        background: ${props => props.theme['green-700']};
+    }
+`
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+    background: ${props => props.theme['red-500']};
+    
+    &:not(:disabled)hover{
+        background: ${props => props.theme['red-700']};
+    }
+`
